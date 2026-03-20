0c267a5007f90f92f83accf45a8fa2ace4a4e9e5
cesarhonda-Git <cesarhonda@usp.br>
2026-03-20 11:23:05 -0300

    Update blink.ino
    3 leds

--- blink.ino
+++ blink.ino
@@ -27,14 +27,21 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(8, OUTPUT);
+  pinMode(11, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(8, LOW);
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(11, LOW);
   digitalWrite(8, HIGH);
-  delay(1000);                       // wait for a second
+  delay(2000); 
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(11, HIGH);
+  digitalWrite(8, LOW);
+  delay(1000);                     // wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);   
+  digitalWrite(11, LOW);
+  digitalWrite(8, LOW);
+  delay(1000);                       
 }
